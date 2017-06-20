d9660269bd81304fa117330032af3740bfd5d5cb
kfujiapple1018 <kfujiapple1018@gmail.com>
2017-06-19 14:58:09 -1000

    Finished

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 function fortuneTeller(){
-	console.log("Hello"); 
+	var numOfChildren = 2;
+	var partnersName = "human";
+	var geographicalLocation = "California";
+	var jobTitle = "Youtuber";
+
+	console.log("You will be a X in Y, and married to Z with N kids.");
+
+	console.log(" You will be a " + jobTitle + "in" + geographicalLocation + " , and married to " + partnersName + "with" + numOfChildren + "kids.");
 }
 
+fortuneTeller();
 function ageCalculator(){
 
 }
